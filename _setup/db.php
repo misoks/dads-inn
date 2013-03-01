670224db017099dd39ad4d86a847fe0364ec76d8
misoks <cathy.a.fisher@gmail.com>
2013-03-01 10:58:21 -0500

    gitignore

--- _setup/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db = mysql_connect("localhost","[USERNAME]", "[PASSWORD]") or die('Fail message');//change to your username/password to test on your machine
-mysql_select_db("[DATABASE NAME]") or die("Fail message");//create itaps db and run createTables, loadData before using these php files
-?>
