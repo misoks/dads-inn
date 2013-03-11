8e73f82ef62bcf65747307fc15af50bded6cad56
misoks <cathy.a.fisher@gmail.com>
2013-03-11 19:08:01 -0400

    event

---
+++ events.php
@@ -0,0 +1,33 @@
+<?php 
+    $page_title = "Events";
+    $page_class = "events page-width--wide";
+    include_once("header.php");
+?>
+
+<?php
+
+?>
+
+<h1>Events</h1>
+
+<p>The Dads Inn hosts events, mixers, commemorations, gatherings, and funerals for its 
+residents throughout the year.<p>
+
+<div class="event">
+    <h2>The 23rd Annual Dads Inn Daddy-Daughter Valentine's Day Dance</h2>
+    <span class="event__date">February 14th, 7 - 10pm</span>
+    <p>Invite your estranged daughter to our annual dance to celebrate the unique 
+        relationship between father and daughter! As usual, the dance will be hosted by
+        our very own former professional radio DJ Mike Kowalcek. He promises he'll be 
+        spinning "all of the late seventies' greatest daddy-daughter classics."</p>
+    <p>Daughter attendance has been disappointing in past
+        years, but event coordinator John Walters says he's been making repeated phone
+        calls to his daughters Helena and Jamie and is sure that they would agree to 
+        come if their mother would just put them on the phone and let him explain 
+        what happened in Orlando.</p>
+    <p>Punch and refreshments will be served in the Ballroom/Conference Center.</p>
+</div>
+
+<?php 
+    include_once("footer.php");
+?>
